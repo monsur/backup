b224e80963e38f7d2ae5d350ca6ac62f2aff05d7
Monsur Hossain <monsur@gmail.com>
2021-01-24 22:42:42 -0600

    Adding paths

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-BACKUP_SRC_DIR=""
-BACKUP_DEST_DIR=""
+BACKUP_SRC_DIR="/Users/monsur"
+BACKUP_DEST_DIR="/Volumes/BACKUP/_BACKUPS/MonsurMacbookPro"
 rsync -avzn --delete $BACKUP_SRC_DIR/Pictures/ $BACKUP_DEST_DIR/Pictures
