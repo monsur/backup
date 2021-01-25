c9876e6f7f8f0a91e5341fb622ce9810218d292f
Monsur Hossain <monsur@gmail.com>
2021-01-24 22:43:18 -0600

    Removing -n

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 BACKUP_SRC_DIR=""
 BACKUP_DEST_DIR=""
-rsync -avzn --delete $BACKUP_SRC_DIR/Pictures/ $BACKUP_DEST_DIR/Pictures
+rsync -avz --delete $BACKUP_SRC_DIR/Pictures/ $BACKUP_DEST_DIR/Pictures
